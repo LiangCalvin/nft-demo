b83027291cf33e541371decd6637f35a1800e0d9
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-27 17:04:55 +0700

    add emit in createCollectible, fulfillRandomness

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -20,6 +20,8 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
     }
     mapping(uint256 => Breed) public tokenIdToBreed;
     mapping(bytes32 => address) public requestIdToSender;
+    event requestedCollectible(bytes32 indexed requestId, address requester);
+    event breedAssigned(uint256 indexed tokenId, Breed breed);
 
     constructor(
         address _vrfCoordinator,
@@ -39,6 +41,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
     function createCollectible() public returns (bytes32) {
         bytes32 requestId = requestRandomness(keyhash, fee);
         requestIdToSender[requestId] = msg.sender;
+        emit requestedCollectible(requestId, msg.sender);
     }
 
     function fulfillRandomness(
@@ -48,6 +51,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
         Breed breed = Breed(randomNumber % 3);
         uint256 newTokenId = tokenCounter;
         tokenIdToBreed[newTokenId] = breed;
+        emit breedAssigned(newTokenId, breed);
         address owner = requestIdToSender[requestId];
         _safeMint(owner, newTokenId);
         // set token uri
