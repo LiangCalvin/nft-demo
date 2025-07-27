e2665a6dd50750caf345a9f24580906b96e15dd2
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-27 16:45:31 +0700

    add advancedcollectible contract

---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,38 @@
+// An NFT Contract
+// Where the tokenURI can be one of these 3 dogs
+// Randomly selected
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyhash;
+    uint256 public fee;
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+    mapping(uint256 => Breed) public tokenIdToBreed;
+    mapping(bytes32 => address) public requestIdToSender;
+
+    constructor(
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _keyhash,
+        uint256 _fee
+    )
+        public
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+        ERC721("Dogie", "DOG")
+    {
+        tokenCounter = 0;
+        keyhash = _keyhash;
+        fee = _fee;
+    }
+}
