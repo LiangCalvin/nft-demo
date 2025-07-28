3362e12ca65320b0e9b042aa9f8dd20fe8df0deb
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-28 16:29:45 +0700

    add upload to ipfs

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -2,6 +2,7 @@ from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
+import requests
 
 
 def main():
@@ -13,7 +14,33 @@ def main():
         metadata_file_name = (
             f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
         )
+        collectible_metadata = metadata_template
         if Path(metadata_file_name).exists():
             print(f" {metadata_file_name} already exist! Delete it to overwrite")
         else:
             print(f"Creating Metadata file: {metadata_file_name}")
+            collectible_metadata["name"] = breed
+            collectible_metadata["description"] = f"An adorable {breed} !"
+            print(collectible_metadata)
+            image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+            image_uri = upload_to_ipfs(image_path)
+            # collectible_metadata["image_uri"] =
+
+
+# curl -X POST -F file=@metadata/sepolia/0-SHIBA_INU.json http://localhost:5001/api/v0/add
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://127.0.0.1:5001"
+        endpoint = "/api/v0/add"
+        response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
+        ipfs_hash = response.json()["Hash"]
+        # "./img/PUG.png" -> "PUG.png"
+        filename = filepath.split("/")[-1:][0]
+        # sample_token_uri = "https://ipfs.io/ipfs/QmbBnUjyHHN7Ytq9xDsYF9sucZdDJLRkWz7vnZfrjMXMxs?filename=2-ST_BERNARD.json"
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        print(image_uri)
+        return image_uri
+        # upload
